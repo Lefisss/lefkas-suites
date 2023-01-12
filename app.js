e8d845b9094ffe08844eabdf6a08ed196204d34e
lefteris <lefterispour92@gmail.com>
2023-01-12 15:49:23 +0200

    commit 12/01/2023

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+
+app.get("/", function (req, res) {
+    res.render("home");
+})
+
+app.get("/home", function (req, res) {
+    res.render("home");
+})
+
+app.get("/about", function (req, res) {
+    res.render("about");
+})
+
+app.get("/apts", function (req, res) {
+    res.render("apts");
+})
+
+app.get("/contact", function (req, res) {
+    res.render("contact");
+})
+
+app.post("/home", function (req, res) {
+    res.render("contact")
+})
+
+app.post("/", function (req, res) {
+    res.render("contact")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function () {
+    console.log("Server start at port 3000.")
+});
