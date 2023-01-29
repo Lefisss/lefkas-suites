43c4cfde0e86f0f16d1bd5ba655900df43f3ca54
lefteris <lefterispour92@gmail.com>
2023-01-29 18:21:17 +0200

    services icons and nodemailer

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+var nodemailer = require('nodemailer');
 
 const app = express();
 
@@ -28,6 +29,60 @@ app.get("/apts", function (req, res) {
 app.get("/contact", function (req, res) {
     res.render("contact");
 })
+
+app.post("/contact", function (req, res) {
+    const fullname = req.body.fullname;
+    const arrival = req.body.arrival;
+    const email = req.body.email;
+    const phonenumber = req.body.phonenumber;
+    const message = req.body.message;
+
+    const fullInquiry = "Ονοματεπώνυμο: " + fullname + "\n" + "Ημερομηνία άφιξης: " + arrival + "\n" + "Στοιχεία επικοινωνίας: " + email + ", " + phonenumber + "\n" + "Μήνυμα: " + message
+
+    console.log(fullInquiry);
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        secure: false,
+        auth: {
+            user: 'lefterispour92@gmail.com',
+            pass: 'dfdkrgwrxmztklng'
+        }
+    });
+
+    var mailOptions = {
+        from: 'lefterispour92@gmail.com',
+        to: 'lefterispournaras@yahoo.gr',
+        subject: 'Πληροφορίες κράτησης για ' + arrival,
+        text: fullInquiry
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+
+    var mailOptions = {
+        from: 'lefterispour92@gmail.com',
+        to: email,
+        subject: 'Επιβεβαίωση αποστολής email',
+        text: "Παραλάβαμε το μήνυμα σας και θα σας απαντήσουμε το συντομότερο δυνατό. Ευχαριστούμε."
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+    res.redirect("/contact");
+})
+
 
 app.post("/home", function (req, res) {
     res.render("contact")
